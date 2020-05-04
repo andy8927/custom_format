9280489148f1b1373f224035e091329ac892172e
andyhsueh <andyhsueh@nine-yi.com>
2020-05-04 18:49:35 +0800

    modify setup content

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ import setuptools
 
 setuptools.setup(
     name="custom_format",
-    version="0.0.7",
+    version="1.0.0",
     author="AndyHsueh",
     author_email="andyhsueh@91app.com",
-    description="A custom format for python_behave_web optional html form",
-    packages=setuptools.find_packages(),
+    description="A custom format for optional html form",
+    packages=["custom_format"],
     python_requires='>=3.6',
 )
