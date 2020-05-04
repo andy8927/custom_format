e1dd4f7e4c2f8c37bc314526c5f0969a080e7842
andyhsueh <andyhsueh@nine-yi.com>
2020-05-04 10:12:05 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="custom_format",
+    version="0.0.7",
+    author="AndyHsueh",
+    author_email="andyhsueh@91app.com",
+    description="A custom format for python_behave_web optional html form",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
