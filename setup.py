5672dd4951cfe839ed65f66cc934a48fe19859a6
andyhsueh <andyhsueh@nine-yi.com>
2020-05-05 17:54:26 +0800

    modify setup content

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name="custom_format",
     version="1.0.1",
     author="AndyHsueh",
-    author_email="andyhsueh@91app.com",
+    author_email="syt156321@gmail.com",
     description="A custom format for optional html form",
     packages=["custom_format"],
     python_requires='>=3.6',
